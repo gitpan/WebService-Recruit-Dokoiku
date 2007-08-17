96bfe7f68e1ff3acbdd093ee4d497e6a44552ccb
Yusuke Kawasaki <u-suke@kawa.net>
2007-08-17 01:19:00 +0000

    Import of KAWASAKI/WebService-Recruit-Dokoiku-0.11 from CPAN.
    gitpan-cpan-distribution: WebService-Recruit-Dokoiku
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         KAWASAKI/WebService-Recruit-Dokoiku-0.11.tar.gz
    gitpan-cpan-author:       KAWASAKI
    gitpan-cpan-maturity:     released

--- lib/WebService/Recruit/Dokoiku.pm
+++ lib/WebService/Recruit/Dokoiku.pm
@@ -2,7 +2,7 @@ package WebService::Recruit::Dokoiku;
 use strict;
 use base qw( Class::Accessor::Fast );
 use vars qw( $VERSION );
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 use WebService::Recruit::Dokoiku::SearchPOI;
 use WebService::Recruit::Dokoiku::GetLandmark;
